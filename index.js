0b66d4e6330917f24ad5fb31e5c5f662eab76980
ahmedgamer4 <aa10@pme.suezuni.edu.eg>
2022-11-25 14:52:14 +0200

    Wow this was too easy

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {'Content-type': 'text/html'})
+      res.write(data)
+      res.end()
+    })
+  }
+
+  else if (req.url === '/about') {
+    fs.readFile('about.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {'Content-type': 'text/html'})
+      res.write(data)
+      res.end()
+    })
+  }
+
+  else if (req.url === '/contact') {
+    fs.readFile('contact-me.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {'Content-type': 'text/html'})
+      res.write(data)
+      res.end()
+    })
+  }
+
+  else {
+    fs.readFile('404.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {'Content-type': 'text/html'})
+      res.write(data)
+      res.end()
+    })
+  }
+}).listen(8082)
