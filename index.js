ae6e8ae8c42cd65038f70a5837346a846ffe42c9
ahmedgamer4 <aa10@pme.suezuni.edu.eg>
2022-12-08 20:29:37 +0200

    made it express

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
 const http = require('http')
 const fs = require('fs')
+const express = require('express')
+const path = require('path')
 
-http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile('index.html', (err, data) => {
-      if (err) throw err
-      res.writeHead(200, {'Content-type': 'text/html'})
-      res.write(data)
-      res.end()
-    })
-  }
+const app = express()
 
-  else if (req.url === '/about') {
-    fs.readFile('about.html', (err, data) => {
-      if (err) throw err
-      res.writeHead(200, {'Content-type': 'text/html'})
-      res.write(data)
-      res.end()
-    })
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
 
-  else if (req.url === '/contact') {
-    fs.readFile('contact-me.html', (err, data) => {
-      if (err) throw err
-      res.writeHead(200, {'Content-type': 'text/html'})
-      res.write(data)
-      res.end()
-    })
-  }
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/about.html'))
+})
 
-  else {
-    fs.readFile('404.html', (err, data) => {
-      if (err) throw err
-      res.writeHead(200, {'Content-type': 'text/html'})
-      res.write(data)
-      res.end()
-    })
-  }
-}).listen(8082)
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact-me.html'))
+})
+
+const errorHandler = (err, req, res, next) => {
+  res.sendFile(path.join(__dirname, '/404.html'))
+}
+
+app.use(errorHandler)
+
+http.createServer(app
+
+  // if (req.url === '/about') {
+  //   fs.readFile('about.html', (err, data) => {
+  //     if (err) throw err
+  //     res.writeHead(200, {'Content-type': 'text/html'})
+  //     res.write(data)
+  //     res.end()
+  //   })
+  // }
+
+  // else if (req.url === '/contact') {
+  //   fs.readFile('contact-me.html', (err, data) => {
+  //     if (err) throw err
+  //     res.writeHead(200, {'Content-type': 'text/html'})
+  //     res.write(data)
+  //     res.end()
+  //   })
+  // }
+
+  // else {
+  //   fs.readFile('404.html', (err, data) => {
+  //     if (err) throw err
+  //     res.writeHead(200, {'Content-type': 'text/html'})
+  //     res.write(data)
+  //     res.end()
+  //   })
+  // }
+).listen(8082)
